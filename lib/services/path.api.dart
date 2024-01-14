eb2756c2022ad579dd7d72089edb8a97bca2a151
Betta Raphael <betta@Bettas-MacBook-Pro-2.local>
2024-01-14 18:59:23 +0100

    fixed bug settings update meel

--- lib/services/path.api.dart
+++ lib/services/path.api.dart
@@ -4,7 +4,7 @@ class APIRoute {
   static String local = 'http://127.0.0.1:4050';
   static String live = 'https://promeal.bubblemeet.online';
 
-  static String base = local;
+  static String base = live;
 
   static String login = "$base/api/account/login";
   static String register = "$base/api/account/register";
